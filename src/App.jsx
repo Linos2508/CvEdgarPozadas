6c96a57b25ae6ff84a60e990615fdda99e09d62d
Ed_poz <epm_1998_eti@hotmail.com>
2021-02-03 00:41:20 -0600

    REFACTOR: All views to a better design

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,12 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import './App.css';
 import Inicio from "./views/inicio.jsx";
-import Sobre from "./views/sobre.jsx";
-import Skills from "./views/skills.jsx";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Route path="/" exact component={Inicio} />
-        <Route path="/sobre" exact component={Sobre} />
-        <Route path="/skills" exact component={Skills} />
       </BrowserRouter>
     );
   }
--- src/views/inicio.jsx
+++ src/views/inicio.jsx
@@ -1,25 +1,37 @@
 import React, { Component } from "react";
 import "./inicio.css";
 import Imagen from "../assets/Profile_Image.jpg";
-import { faEnvelope, faPhone, faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
-import { faFacebook, faGithub } from "@fortawesome/free-brands-svg-icons"
+import { faEnvelope, faPhone, faMapMarkerAlt, faDatabase, faDownload } from "@fortawesome/free-solid-svg-icons";
+import { 
+    faFacebook,
+    faGithub, 
+    faLinkedin,
+    faJs,
+    faPhp,
+    faHtml5,
+    faCss3,
+    faSass,
+    faReact,
+    faAngular,
+    faPython, 
+    faUbuntu,
+    faLinux,
+    faBootstrap} from "@fortawesome/free-brands-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import Navegacion from "../components/navigation.jsx";
+import PDF from "../assets/cv_edgar.pdf"
 
 class Home extends Component {
-    state = {
-        //boot: getBoot()[getCookie("Orolang")],
-    };
-    componentDidMount() {
-    }
+    state = {};
+    componentDidMount() {}
     render () { 
         return(
             <>
-                <Navegacion></Navegacion>
                 <section className="presentacion">
                     <div className="contenedorPrincipal">
                         <div className="informacion">
-                            <button>Hola yo soy</button>
+                            <div className="imagen">
+                                <img src={Imagen} alt="yo"/>
+                            </div>
                             <h1>Edgar Pozadas</h1>
                             <h3>Desarrollador Full-Stack</h3>
                             <p><FontAwesomeIcon icon={faEnvelope}/> <a href="mailto:epm_1998_eti@hotmail.com">epm_1998_eti@hotmail.com</a></p>
@@ -28,10 +40,42 @@ class Home extends Component {
                             <div className="socialMedia">
                                 <a href="https://www.facebook.com/edgar.pozadas/"><FontAwesomeIcon icon={faFacebook} /></a>
                                 <a href="https://github.com/Linos2508"><FontAwesomeIcon icon={faGithub} /></a>
+                                <a href="https://www.linkedin.com/in/edgar-pozadas-5261b4187/"><FontAwesomeIcon icon={faLinkedin} /></a>
+                                <a href={PDF} target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faDownload} /></a>
                             </div>
                         </div>
-                        <div className="imagen">
-                            <img src={Imagen} alt="yo"/>
+                        <div className="masInfo">
+                            <div className="objetivos">
+                                <h2> Objetivo Profesional</h2>
+                                <p>
+                                    Encontrar una empresa de TI donde pueda seguir desarrollando mis habilidades y aptitudes.
+                                    Adicto al código, Amante de Python
+                                </p>
+                            </div>
+                            <div className="skillsTecnicos">
+                                <h2>Skills Técnicos</h2>
+                                <div className="lenguajes">
+                                    <FontAwesomeIcon icon={faPython} title="Python3"/>
+                                    <FontAwesomeIcon icon={faPhp} title="PHP" />
+                                    <FontAwesomeIcon icon={faDatabase} title="Databases" />
+                                    <FontAwesomeIcon icon={faUbuntu} title="Ubuntu" />
+                                    <FontAwesomeIcon icon={faLinux} title="Linux" />
+                                    <FontAwesomeIcon icon={faJs} title="JavaScript" />
+                                    <FontAwesomeIcon icon={faReact} title="ReactJs" />
+                                    <FontAwesomeIcon icon={faAngular} title="Angular" />
+                                    <FontAwesomeIcon icon={faHtml5} title="HTML5" />
+                                    <FontAwesomeIcon icon={faCss3} title="CSS" />
+                                    <FontAwesomeIcon icon={faBootstrap} title="Bootstrap" />
+                                    <FontAwesomeIcon icon={faSass} title="SASS (CSS with superpowers)" />
+                                </div>
+                            </div>
+                            <div className="experienciaLaboral">
+                                <h2>Experiencia Laboral</h2>
+                                <ul>
+                                    <li>Web Full-Stack Developer y Líder Tecnico en Platimex Enero 2020 – Actualidad.</li>
+                                    <li>Web Full-Stack Developer en Crown Clever Company Enero 2019 – Diciembre 2019.</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </section>
